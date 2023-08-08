0f004f04d35c9d2936caf91af0f8aae7aa4dda7a
Woojin Kwak <kwooj2788@kookmin.ac.kr>
2023-08-08 11:33:45 +0900

    [FEAT] 키바인딩용 Key Controller 추가

---
+++ source/Assets/Scripts/System/KeyController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyController : MonoBehaviour
+{
+    /// <summary>
+    /// VARIABLES
+    /// </summary>
+    private UIManager ui;
+
+    /// <summary>
+    /// EVENT FUNCTIONS
+    /// </summary>
+
+    void Start()
+    {
+        ui = FindObjectOfType<UIManager>();
+    }
+    
+    void Update()
+    {
+        // S - INFO OPEN
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            ui.ManageInfo();
+        }
+        // Q - QUEST OPEN
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            ui.ManageQuest();
+        }
+    }
+}
