6ff866aa39966675b765b08eebcb1106f3fc7ec9
Woojin Kwak <kwooj2788@kookmin.ac.kr>
2023-08-11 10:43:17 +0900

    [FEAT] 설정 데이터 클래스 / 직렬화 저장 기능 구현

---
+++ source/Assets/Scripts/System/Setting.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class Setting
+{
+    public General general;
+    public Audio audio;
+    public Control control;
+
+    public class General
+    {
+        public string screen;
+        public string language;
+    }
+
+    public class Audio
+    {
+        public float master;
+        public float bgm;
+        public float effect;
+    }
+
+    public class Control
+    {
+        public float sensitivity;
+        public string sprint;
+        public string interact;
+        public string info;
+        public string quest;
+        public string belongings;
+    }
+
+    public Setting()
+    {
+        this.general = new General()
+        {
+            screen = "FullScreen",
+            language = "en-US"
+        };
+        this.audio = new Audio()
+        {
+            master = 1f,
+            bgm = 1f,
+            effect = 1f
+        };
+        this.control = new Control()
+        {
+            sensitivity = 1f,
+            sprint = "LeftShift",
+            interact = "F",
+            info = "S",
+            quest = "Q",
+            belongings = "E"
+        };
+    }
+}
--- source/Assets/Scripts/System/SettingController.cs
+++ source/Assets/Scripts/System/SettingController.cs
@@ -1,22 +1,71 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Michsky.MUIP;
+using System;
+using System.IO;
+using System.Windows.Forms;
+using UnityEditor.Localization.Plugins.XLIFF.V20;
+using Application = UnityEngine.Application;
+using Screen = UnityEngine.Screen;
 
 public class SettingController : MonoBehaviour
 {
     /// <summary>
     /// VARIABLES
     /// </summary>
+    private string dataPath;
 
+    public Setting currentSetting;
 
     /// <summary>
     /// EVENT FUNCTIONS
     /// </summary>
     
-    void Update()
+    void Start()
     {
-        
+        dataPath = Application.persistentDataPath + "/settings.json";
+        LoadData();
+    }
+
+    /// <summary>
+    /// LOAD / SAVE FUNCTIONS
+    /// </summary>
+
+    public void SaveData(Setting target)
+    {
+        string jsonData = JsonUtility.ToJson(target);
+        File.WriteAllText(dataPath, jsonData);
     }
     
+    public void LoadData()
+    {
+        // settings.json 파일이 존재하면 읽어 오기
+        if(File.Exists(dataPath))
+        {
+            try
+            {
+                string jsonData = File.ReadAllText(dataPath);
+                currentSetting = JsonUtility.FromJson<Setting>(jsonData);
+            }
+            catch
+            {
+                string message = "Your option file is broken. It'll be reseted. Please run again.";
+                string caption = "Fatal Error!";
+                MessageBoxButtons button = MessageBoxButtons.OK;
+                MessageBoxIcon icon = MessageBoxIcon.Error;
+                DialogResult result = MessageBox.Show(message, caption, button, icon);
+                if (result == DialogResult.OK)
+                {
+                    File.Delete(dataPath);
+                    Application.Quit();
+                }
+            }
+        }
+        // 존재하지 않으면 기본값의 설정 파일 생성
+        else
+        {
+            currentSetting = new Setting();
+            SaveData(currentSetting);
+        }
+    }
 }
