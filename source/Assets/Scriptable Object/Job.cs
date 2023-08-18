789e9c13255bc97063d14a95dc71e719b7b8cfff
biyodde <billjhkim@kookmin.ac.kr>
2023-08-18 19:23:57 +0900

    [FEAT] PlayerManager 구현

---
+++ source/Assets/Scriptable Object/Job.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Job", menuName = "Scriptable Object/Job", order = int.MaxValue)]
+public class Job : ScriptableObject
+{
+    
+}
---
+++ source/Assets/Scripts/GamePlay/PlayerManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    /// <summary>
+    /// VARIABLES
+    /// </summary>
+    public float money;
+    public Job currentJob;
+    public int Knowledge;
+    public int Luck;
+    public int Strength;
+    public int Driving;
+    public int Reputation;
+    public int Faith;
+    public int Life;
+    public int Stamina;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
